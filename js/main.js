2190b85df213b7388623d3721f84e0d01a0bc563
cstapiarubio <cstapiarubio@gmail.com>
2017-07-28 23:28:40 -0400

    lista nav html preliminar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
+/*inicio fx jquery*/
 $(document).ready(function(){
+	/*recorro con forEach mi var pint (json con info de pinterest)*/
 	pint.forEach(function(el){
-	console.log(el);
-	var id= el.id;
-	var img= $('<img>', {'src':'..img/'+ id +'.jpg'});
-	$("#contenedor").append("<div id='pinterest'>"+ '<h2>'+ el.title +'</h2>'+ 
-		'<h2>'+ el.user +'</h2>'+ '<h2>'+ el.description +'</h2>'+'<h2>'+ el.hashtag 
-		+'</h2>'+"<img src='img/" + id + ".jpg' ><div>");		
-				});
+		/*muestra en consola el arreglo. Es para corroborar lo que estoy haciendo*/
+		console.log(el);
+		/*obtengo el id de cada publicación desde json y lo guardo en una variable*/
+		var id= el.id;
+		/*var img= $('<img>', {'src':'..img/'+ id +'.jpg'});*/
+
+	/*info que quiero la llamo (en este caso) con el.loquequiero; ejem el.title; el.user etc
+	y la añado con un append a mi div contenedor que esta señalado en el html. 
+	La info que quiero la guardo dentro de div, h1, img según sea el caso*/
+	$("#contenedor").append("<div id='pinterest'>"+ '<h4>'+ el.title +'</h4>'+ 
+		'<p>'+ el.description +'</p>'+ '<p>'+ el.user +'</p>'+'<p>'+ el.hashtag +'</p>'
+		/*la img la inserto en una etiqueta IMG y copio su ruta, en este caso carpeta img/foto.jpg;
+		luego la llamo traves del id y le añado la extension jpg en este caso*/
+		+"<img src='img/" + id + ".jpg' ><div>");		
+});
 })

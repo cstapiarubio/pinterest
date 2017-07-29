5824ccd8918e87c47e325ebaca5017a375a0aba0
cstapiarubio <cstapiarubio@gmail.com>
2017-07-29 00:02:29 -0400

    maquetacion lista, falta mas diseño

--- dest/js/todo.js
+++ dest/js/todo.js
@@ -10268,10 +10268,11 @@ $(document).ready(function(){
 	/*info que quiero la llamo (en este caso) con el.loquequiero; ejem el.title; el.user etc
 	y la añado con un append a mi div contenedor que esta señalado en el html. 
 	La info que quiero la guardo dentro de div, h1, img según sea el caso*/
-	$("#contenedor").append("<div id='pinterest'>"+ '<h4>'+ el.title +'</h4>'+ 
-		'<p>'+ el.description +'</p>'+ '<p>'+ el.user +'</p>'+'<p>'+ el.hashtag +'</p>'
-		/*la img la inserto en una etiqueta IMG y copio su ruta, en este caso carpeta img/foto.jpg;
+
+	/*la img la inserto en una etiqueta IMG y copio su ruta, en este caso carpeta img/foto.jpg;
 		luego la llamo traves del id y le añado la extension jpg en este caso*/
-		+"<img src='img/" + id + ".jpg' ><div>");		
+	$("#contenedor").append("<li id='pinterest'>"+ "<img src='img/" + id + ".jpg' >" + 
+		'<h4>'+ el.title +'</h4>'+ '<p>'+ el.description +'</p>'+ '<p>'+ el.user +'</p>'+
+		'<p>'+ el.hashtag +'</p>' + "<li>");		
 });
 })

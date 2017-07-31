72d888eb51f4e8b817676647adc5347a4b18e3f8
cstapiarubio <cstapiarubio@gmail.com>
2017-07-31 14:33:24 -0400

    listo maquetacion de fotos

--- js/main.js
+++ js/main.js
@@ -14,39 +14,9 @@ $(document).ready(function(){
 
 	/*la img la inserto en una etiqueta IMG y copio su ruta, en este caso carpeta img/foto.jpg;
 		luego la llamo traves del id y le añado la extension jpg en este caso*/
-	$("#contenedor").append("<li id='pinterest'>"+ "<img id='imag' onclick='onClick(this)' src='img/" + id + ".jpg' >" + 
-		'div id="myModal" class="modal">' + '<div class="modal-content">' + '<span class="close">'+
-		'&times;'+ '</span>' + '<p>' + "some text in the modal"+
-'</p>' +'</div>' +'</div>' + '<h4>'+ el.title +'</h4>'+ '<p>'+ el.description +'</p>'+ '<p>'+ el.user +'</p>'+
-		'<p>'+ el.hashtag +'</p>' + "<li>");		
+	$("#contenedor").append("<li id='pinterest'>"+ "<img onclick='onClick(this)' src='img/" + id + ".jpg' >" + 
+ '<h4>'+ el.title +'</h4>'+ '<p>'+ el.description +'</p>'+ '<p>'+ el.user +'</p>'+
+		'<p>'+ '#'+ el.hashtag +'</p>' + "<li>");		
 });
 })
 
-/*pag modal sobre img 
-https://www.w3schools.com/howto/howto_css_modal_images.asp*/
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var imagen = document.getElementById("imag");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-imagen.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
